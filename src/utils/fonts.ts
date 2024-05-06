20c32daab3bd62ad5f2c2f47d8d933313ccfc73e
gyeong3un2 <gyeong3un2@gmail.com>
2024-05-07 00:05:11 +0900

    feat: Gnb 컴포넌트 퍼블리싱

---
+++ src/utils/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Noto_Sans_KR } from "next/font/google";
+
+export const inter = Inter({ 
+  subsets: ['latin'],
+});
+
+export const noto_sans_kr = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: '500',
+})
