aabec6ede230a4c8b9c092b75f557fe9f9d9e4a9
경은 <gyeong3un2@gmail.com>
2024-05-07 14:56:49 +0900

    feat: 검색창, 검색결과 없음 퍼블리싱

--- src/assets/fonts.ts
+++ src/assets/fonts.ts
@@ -1,10 +1,10 @@
-import { Inter, Noto_Sans_KR } from "next/font/google";
+import { Inter, Noto_Sans_KR } from 'next/font/google';
 
-export const inter = Inter({ 
+export const inter = Inter({
   subsets: ['latin'],
 });
 
 export const noto_sans_kr = Noto_Sans_KR({
   subsets: ['latin'],
-  weight: '500',
-})
+  weight: ['300', '500', '700', '800', '900'],
+});
